cafc33c86d877225ec7e7abebdc063f3d883c862
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-08-12 00:42:33 +0200

    feat: first commit

---
+++ apps/docs/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<header>
+    <nav>
+        <a href="/">MVP UI</a>
+        <menu>
+            <li><a href="/docs">Docs</a></li>
+            <li><input type="search" placeholder="Search" /></li>
+        </menu>
+    </nav>
+</header>
+<main><slot /></main>
+<footer></footer>
---
+++ apps/example/main.js
@@ -0,0 +1,1 @@
+import "@mvp-ui/main";
