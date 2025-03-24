b1adfba85fdc93775698c64fc65977511843dcc3
Torsten Dittmann <torsten.dittmann@googlemail.com>
2025-03-24 21:17:26 +0100

    Update theme switching and add light theme
    The changes switch from class-based to data-attribute theming, simplify theme
    switcher controls, and add light theme styles. Also includes minor HTML and
    form input improvements.

--- apps/example/main.js
+++ apps/example/main.js
@@ -1,1 +1,10 @@
 import "@dittmann/ui";
+
+for (const element of document.querySelectorAll("[data-theme-switcher]")) {
+	element.addEventListener("click", () => {
+		document.documentElement.setAttribute(
+			"data-theme",
+			element.dataset.themeSwitcher,
+		);
+	});
+}
